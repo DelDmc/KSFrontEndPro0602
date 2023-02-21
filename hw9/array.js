d222bf0f76445b1138a74203b923538fd0ce5339
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-02-21 23:59:01 +0200

    Corrected as per comments

--- hw9/array.js
+++ hw9/array.js
@@ -3,10 +3,31 @@ let userArray = ['alpha', 'beta', 'delta', 'hanzel', 'and', 'grettel'];
 console.log(userArray);
 
 // Відсортувати масив за зростанням.
-userArray.sort((a, b) => a.length - b.length);
+function compareNumeric(a, b) {
+    if (a > b) {
+        console.log(a, b);
+        return 1;
+    }		
+    if (a === b) {
+        console.log(a, b);
+        return 0;
+    }	
+    if (a < b) {
+        console.log(a, b);
+        return -1;
+    }	
+  }
+
+userArray.sort(
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a === b) return 0;
+        if (a < b) return -1;
+      }
+);
 console.log(userArray);
 
 // Видалити елементи з масиву з 2 по 4 (включно!).
-userArray.splice(1, 3);
+userArray.splice(2, 3);
 console.log(userArray);
 
