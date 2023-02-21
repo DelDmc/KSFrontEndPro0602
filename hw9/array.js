78ae89c156427c67e490f078debab839faf860d2
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-02-22 00:10:24 +0200

    minor corrections

--- hw9/array.js
+++ hw9/array.js
@@ -3,16 +3,14 @@ let userArray = ['alpha', 'beta', 'delta', 'hanzel', 'and', 'grettel'];
 console.log(userArray);
 
 // Відсортувати масив за зростанням.
-userArray.sort(
-    function compareNumeric(a, b) {
-        if (a > b) return 1;
-        if (a === b) return 0;
-        if (a < b) return -1;
-      }
-);
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a === b) return 0;
+    if (a < b) return -1;
+  }
+userArray.sort(compareNumeric);
 console.log(userArray);
 
 // Видалити елементи з масиву з 2 по 4 (включно!).
 userArray.splice(2, 3);
 console.log(userArray);
-
